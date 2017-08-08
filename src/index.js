066089ea5beb42f91c57d9687b25a38ddd8fc024
Bishop O'Connell <bishopoconnell1@gmail.com>
2017-08-07 20:49:58 -0400

    added component files and imported a search bar component into index.js and rendered it to the DOM

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const destination = document.querySelector('.container');
+const API_KEY = 'AIzaSyA1pq60s8Z9VQZlDE2Z-I8KEcrvXFYmQyg';
 
-const App = () => {
-  return (
-    <div>Hello Earth?</div>
-  );
-};
+const App = () => <div><SearchBar /></div>;
 
 ReactDOM.render(<App />, destination);
