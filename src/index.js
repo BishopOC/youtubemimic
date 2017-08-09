e7f55774c1c21a84e9cefbe7eed4aa95e4c505cc
Bishop O'Connell <bishopoconnell1@gmail.com>
2017-08-09 17:06:26 -0400

    recreated git repo to hide API Key

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import API_KEY from '../secret.js';
 const destination = document.querySelector('.container');
-const API_KEY = 'AIzaSyA1pq60s8Z9VQZlDE2Z-I8KEcrvXFYmQyg';
 
-const App = () => <div><SearchBar /></div>;
+YTSearch({ key: API_KEY, term: 'surfboards' }, function (data) {
+  console.log(data);
+});
+
+class App extends Component {
+  render() {
+    return (
+    <div>
+      <SearchBar />
+    </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, destination);
