43b163a4fe213d86546ac114c2023331a77e9234
Bishop O'Connell <bishopoconnell1@gmail.com>
2017-08-10 17:57:19 -0400

    Created a functional component in video_list.js that takes in a props argument. This component renders a simple ul and lists the length of the video list. In index.js, an instance of video_list is rendered where the props "videos" is created and passed in the current state.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-import API_KEY from '../secret.js';
+import API_KEY from '../secret';
+import VideoList from './components/video_list';
 const destination = document.querySelector('.container');
 
 class App extends Component {
@@ -20,6 +21,7 @@ class App extends Component {
     return (
     <div>
       <SearchBar />
+      <VideoList videos={this.state.videos} />
     </div>
     );
   }
